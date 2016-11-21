fe3df403f81ca7242a3f45c96e9c01593ce88fb2
Ben Healey <Healey94@hotmail.co.uk>
2016-11-21 17:28:03 +0000

    Camera System Being Implemented

---
+++ Test Level/Assets/Scripts/Spotted_Test.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spotted_Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisonEnter(Collider col){
+		Debug.Log ("col");
+		if (col.gameObject.name == "Player") {
+			Debug.Log ("Enters Camera Space");
+			//TestLight.enabled = true;
+		}
+	}
+
+	void OnTriggerEnter(Collider col) {
+		Debug.Log ("col");
+		if (col.gameObject.tag == "Player") {
+			Debug.Log ("Enters Camera Space");
+			//estLight.enabled = true;
+		}
+	}
+}
