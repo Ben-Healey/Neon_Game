21d8274978f0464212640fb643490b338455d9e0
Ben Healey <Healey94@hotmail.co.uk>
2017-02-08 14:59:51 +0000

    Start Texturing of Level and Player Animations

--- Tutorial Level/Assets/Scripts/Gameplay Scripts/Player Scripts/playerMovement.cs
+++ Tutorial Level/Assets/Scripts/Gameplay Scripts/Player Scripts/playerMovement.cs
@@ -5,37 +5,27 @@ using System.Collections;
 
 [RequireComponent (typeof(CharacterController))]
 public class playerMovement : MonoBehaviour {
-
-	public Attributes Att_Script;
-	//float Speed_Test = Att_Script.Get_Player_Speed();
-
+	
 	public float walkSpeed = 6.0f;
 	public float runSpeed = 11.0f;
 
 	// Normally, moving diagonally will multiply forwards and side movement moving you at 1.4x speed at diagonal
 	public bool limitDiagonalSpeed = true;
-
 	// Allow a switch between toggle or hold run key - bind "Run" in InputManager
 	public bool toggleRun = true;
-
 	public float jumpSpeed = 8.0f;
 	public float gravity = 20.0f;
-
 	// How many units can you fall before taking fall damage (change to "infinity" in Inspector to disable)
 	public float fallDamageLimit = 10.0f;
-
 	// Prevents standard character controllers from bouncing when moving on slopes
 	public float antiBumpFactor = .75f;
-
 	// Number of physics frames you must be grounded before jumping again - 0 enables BH
 	public int antiBunnyhopFactor = 1;
-
 	private Vector3 moveDirection = Vector3.zero; // Vector3.zero == Vector3(0, 0, 0)
 	private bool grounded = false; // Check whether the player is on a floor
 	private CharacterController controller;
 	private Transform myTransform;
 	private float speed;
-
 	private float fallStartHeight;
 	private bool falling;
 	private int jumpTimer;
@@ -47,8 +37,8 @@ public class playerMovement : MonoBehaviour {
 		jumpTimer = antiBunnyhopFactor;
 		Cursor.lockState = CursorLockMode.Confined; // keep confined in the game window
 		Cursor.lockState = CursorLockMode.Locked;   // keep confined to center of screen
-		Cursor.lockState = CursorLockMode.None;     // set to default default
-		// rayDistance = controller.height * .5f + controller.radius;
+		Cursor.lockState = CursorLockMode.None;     // set to default 
+
 	}
 
 	void FixedUpdate () {
@@ -70,7 +60,7 @@ public class playerMovement : MonoBehaviour {
 
 			moveDirection = new Vector3 (inputX * inputModifyFactor, -antiBumpFactor, inputY * inputModifyFactor);
 			moveDirection = myTransform.TransformDirection (moveDirection) * speed;
-			//			playerControl = true;
+
 
 			// Jumping controls are defined by whether the button is not pressed and that the time delay has been reached
 			if (!Input.GetButton ("Jump"))
@@ -79,14 +69,12 @@ public class playerMovement : MonoBehaviour {
 				moveDirection.y = jumpSpeed;
 				jumpTimer = 0;
 			}
-		}
-		else {
+		} else {
 			if (!falling) { // Check every frame whether we are falling, as it gets reset when we are grounded
 				falling = true;
 				fallStartHeight = myTransform.position.y;
 			}
 		}
-
 		// Apply gravity movement
 		moveDirection.y -= gravity * Time.deltaTime;
 
