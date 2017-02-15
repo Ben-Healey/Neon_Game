5afdef80461dd6aaa6efa8308ca4b6e4b0506eff
Joshua Jones <joshuaphilipjones36@gmail.com>
2017-02-15 00:44:20 +0000

    Basic EnemyAI is added
    Framework for enemyAI is implemented, still needs cover checking and
    leaning around cover - state machine implementation.

---
+++ Tutorial Level/Assets/Scripts/Enemy Scripts/Enemy_Find_Cover.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Find_Cover : MonoBehaviour {
+
+    public UnityEngine.AI.NavMeshAgent agent;
+    public List<GameObject> coverList = new List<GameObject>(); // All nearby covers
+    public List<GameObject> validCover = new List<GameObject>(); // Covers that return as valid
+
+	void Start () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	void FixedUpdate () {
+		for (int i = 0; i < coverList.Count; i++) {
+            // Check if the cover is valid
+            if (coverList[i].GetComponent<coverEnabler>().CheckValid()) {
+                Debug.Log("Cover is valid");
+                // Cover will need to be added to another list of valid covers, this list will be searched for closest and move to it.
+                validCover.Add(coverList[i]);
+            }
+        }
+        // Function checks all valid cover points to find the one closest to itself and sets that as its destination.
+        agent.SetDestination(MoveToCover());
+    }
+
+    // Add the detected cover to the list of considered covers
+    void OnTriggerEnter (Collider _collision) {
+        if (_collision.gameObject.CompareTag("Cover"))
+            coverList.Add(_collision.gameObject);
+    }
+
+    // Remove the out-of-range cover from the local list of covers
+    void OnTriggerExit (Collider _collision) {
+        if (_collision.gameObject.CompareTag("Cover"))
+            coverList.Remove(_collision.gameObject);
+    }
+
+    Vector3 MoveToCover () {
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        
+        foreach(GameObject cover in validCover) {
+            Vector3 directionToTarget = cover.transform.position - transform.position;
+            float distSqrToTgt = directionToTarget.sqrMagnitude;
+            if (distSqrToTgt < closestDistanceSqr) {
+                closestDistanceSqr = distSqrToTgt;
+                bestTarget = cover.transform;
+            }
+        }
+
+        if (bestTarget == null) {
+            bestTarget = transform;
+            Debug.Log("bestTarget null -> no target");
+        }
+
+        return bestTarget.position;
+    }
+}
---
+++ Tutorial Level/Assets/Scripts/Enemy Scripts/coverEnabler.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coverEnabler : MonoBehaviour {
+
+    public bool isValid = false;
+    public bool squadValid = false;
+    public bool playerValid = false;
+
+    GameObject player;
+    GameObject[] squad;
+
+    Ray checkRay;
+    RaycastHit hit;
+    
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        squad = GameObject.FindGameObjectsWithTag("Squad");
+
+        // Will never move position so may as well set here then update direction as needed
+        checkRay.origin = gameObject.transform.position;
+	}
+
+    public bool CheckValid () {
+        checkPlayer();
+        checkSquad();
+
+        if(playerValid && squadValid) {
+            Debug.Log("playerValid && squadValid passed");
+            isValid = true;
+        }
+
+        Debug.Log("isValid = " + isValid);
+        return isValid;
+    }
+
+    bool checkPlayer () {
+        // Move raycast towards player direction
+        checkRay.direction = player.transform.position - gameObject.transform.position;
+
+        Physics.Raycast(checkRay, out hit, 50f); // Raycast towards player for 50 units
+
+        if (!hit.collider.CompareTag("Player")) // If you don't hit the player,
+            playerValid = true;                 // it's a valid cover.
+
+        Debug.Log("playerValid = " + playerValid);
+        return playerValid;
+    }
+
+    bool checkSquad () {
+        //for(int i = 0; i < squad.Length; i++) {
+        //    if(squad[i] == null)
+        //        squad[i].
+        //}
+
+        bool looping = true;
+        bool squadInvalid = false;
+
+        for (int i = 0; i < squad.Length; i++) {
+            while (looping) {
+                // Move raycast towards squad member [i]
+                checkRay.direction = squad[i].transform.position - gameObject.transform.position;
+
+                Physics.Raycast(checkRay, out hit, 50f);
+                if (hit.collider.CompareTag("Squad")) {
+                    looping = false;
+                    Debug.Log("looping should be false = " + looping);
+                    squadInvalid = true;
+                }
+            }
+        }
+
+        if (!squadInvalid)
+            squadValid = true;
+
+        Debug.Log("squadValid = " + squadValid);
+        return squadValid;
+    }
+}
