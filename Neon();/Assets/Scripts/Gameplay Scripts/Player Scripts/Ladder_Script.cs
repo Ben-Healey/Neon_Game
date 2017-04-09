bc0847e35a9a4be157f48799384925f7d05871d4
Ben Healey <Healey94@hotmail.co.uk>
2017-04-09 14:30:54 +0100

    Ladder Sort of Usable

---
+++ Neon();/Assets/Scripts/Gameplay Scripts/Player Scripts/Ladder_Script.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder_Script : MonoBehaviour
+{
+    bool canClimb = false;
+    GameObject player;
+    Rigidbody rb;
+    playerMovement player_move;
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rb = player.GetComponent<Rigidbody>();
+        player_move = player.GetComponent<playerMovement>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player_move.enabled = false;
+            Debug.Log("Player in ladder");
+            canClimb = true;
+            //if(Input.GetKeyDown(KeyCode.W))
+            //{
+            //    //Debug.Log("SHOULD GO UP!");
+            //    //other.transform.Translate(Vector3(0,1,0) *  Time.deltaTime);
+            //}
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            canClimb = false;
+        }
+    }
+
+    void Update()
+    {
+        if(canClimb == true)
+        {
+            if(Input.GetKeyDown(KeyCode.W))
+            {
+                //Debug.Log("BUTTON PRESS");
+                rb.AddForce(Vector3.up * 200);
+            }
+            if(Input.GetKeyDown(KeyCode.S))
+            {
+                rb.AddForce(Vector3.down * 200);
+            }
+        }
+    }
+
+}
